932656003bccbd3831b7a44a2d5246831b73133a
oudne3172 <80808137+oudne3172@users.noreply.github.com>
2021-04-14 14:22:22 +0900

    123

--- Assets/Scripts/ScrollingObject.cs
+++ Assets/Scripts/ScrollingObject.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScrollingObject : MonoBehaviour
-{
+public class ScrollingObject : MonoBehaviour {
     public float speed = 10f;
 
     private void Update()
     {
-        transform.Translate(Vector3.left * speed * Time.deltaTime);
-    }
+        if (!GameManager.instance.isGameover)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+    }   
 }
